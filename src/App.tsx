33b286bc026b94d713dd0716750ed05be6c1de19
StonedHesus <andrei.ionescu@etu.u-paris.fr>
2023-04-06 18:52:18 +0200

    Concluded the issue.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          🚧 Building in progress<em className="animated-ellipses">...</em>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
